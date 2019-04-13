97d51e604b9c35b5a7b8d5cf60fc65a7700cc126
michael allen <michael.allen.forward@gmail.com>
2019-04-12 19:16:55 -0500

    init .sln commit

---
+++ src/BenchmarkMiddleWare/Calc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BenchmarkMiddleWare
+{
+    public class Calc
+    {
+        public int Addition(int int1, int int2)
+        {
+            return int1 + int2;
+        }
+    }
+}
